d623f72a037c222fc2647d709cd17963fa2b6cbd
Matthew Church <churchmf@gmail.com>
2019-01-25 21:15:28 -0700

    Adding script for testing navmeshagent pathfinding

---
+++ Assets/Scripts/MoveToClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClick : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent _agent = null;
+    [SerializeField]
+    LayerMask _mask = -1;
+    [SerializeField]
+    float _castDistance = 100f;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(mouseRay, out hit, _castDistance, _mask))
+            {
+                NavMeshHit navHit;
+                if(NavMesh.SamplePosition(hit.point, out navHit, _castDistance, _mask))
+                {
+                    _agent.SetDestination(navHit.position);
+                }
+            }
+        }
+    }
+}
