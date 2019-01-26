6e26463150ed5b7148e92cde409766b166fc3220
Matthew Church <churchmf@gmail.com>
2019-01-25 21:26:55 -0700

    Adding enemy spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent _enemyPrefab = null;
+    [SerializeField]
+    float _startAfterSeconds = 10f;
+    [SerializeField]
+    float _spawnRateSeconds = 10f;
+    [SerializeField]
+    Transform _destination = null;
+    [SerializeField]
+    LayerMask _mask = -1;
+
+    void Awake()
+    {
+        InvokeRepeating("Spawn", _startAfterSeconds, _spawnRateSeconds);
+    }
+
+    void Spawn()
+    {
+        NavMeshAgent enemy = GameObject.Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(_destination.position, out hit, 10f, _mask))
+        {
+            enemy.SetDestination(hit.position);
+        }
+        else
+        {
+            Debug.LogError("Couldn't find nav mesh position!");
+        }
+    }
+}
