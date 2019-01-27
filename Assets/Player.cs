65223ad3dacbd81ecad6d90147836c92bd3d2364
Matthew Church <churchmf@gmail.com>
2019-01-27 11:38:26 -0700

    Added bear roaming

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,13 +1,18 @@
-﻿using Sirenix.OdinInspector;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(SpriteRenderer))]
-public class Enemy : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    [SerializeField, MinValue(1)]
-    int _homeDamage = 1;
+    [SerializeField]
+    Transform _home = null;
+    [SerializeField]
+    float _goHomeStartSeconds = 1f;
+    [SerializeField]
+    float _goHomeRepeatSeconds = 3f;
+    [SerializeField]
+    float _randomDistance = 1f;
     [SerializeField]
     Sprite _horizontal = null;
     [SerializeField]
@@ -17,13 +22,19 @@ public class Enemy : MonoBehaviour
 
     NavMeshAgent _agent = null;
     SpriteRenderer _spriteRenderer = null;
-
-    public int GetHomeDamage() { return _homeDamage; }
-
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        InvokeRepeating("GoHome", _goHomeStartSeconds, _goHomeRepeatSeconds);
+    }
+
+    void Update()
+    {
+        if(!_agent.hasPath || _agent.desiredVelocity.magnitude <= 0f)
+        {
+            _agent.SetDestination(RandomNavMesh());
+        }
     }
 
     void LateUpdate()
@@ -39,7 +50,6 @@ public class Enemy : MonoBehaviour
             else
             {
                 _spriteRenderer.flipX = false;
-
             }
         }
         else
@@ -53,5 +63,21 @@ public class Enemy : MonoBehaviour
                 _spriteRenderer.sprite = _back;
             }
         }
+    }
+
+    void GoHome()
+    {
+        _agent.SetDestination(_home.position);
+    }
+
+    Vector3 RandomNavMesh()
+    {
+        Vector3 random = _home.position;
+        var randomDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+        if(NavMesh.SamplePosition(transform.position + (randomDirection * _randomDistance), out var hit, 1f, -1))
+        {
+            random = hit.position;
+        }
+        return random;
     }
 }
